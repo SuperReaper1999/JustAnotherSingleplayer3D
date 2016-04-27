958d6828024bb6772e09c62223d5ad4a580c3728
SuperReaper1999 <SuperReaper1999@gmail.com>
2016-04-27 20:49:06 +0100

    Added assets.
    added unity's standard characters and particles and also added my
    Standard menu system.

---
+++ Assets/SuperReaperAssets/MenuPackage/Menu_Scripts/MenuButtons.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MenuButtons : MonoBehaviour {
+
+    // Disables all currently active canvases.
+    public void ResumeGame () {
+        GameObject settingsMenu = GameObject.Find("SettingsMenu");
+        settingsMenu.GetComponent<Canvas>().enabled = false;
+
+        GameObject pauseMenu = GameObject.Find("PauseMenu");
+        pauseMenu.GetComponent<Canvas>().enabled = false;
+    }
+
+    // Closes the game.
+    public void ExitGame () {
+        Application.Quit();
+    }
+
+    // Opens the settings menu.
+    public void SettingsButton () {
+        GameObject settingsMenu = GameObject.Find("SettingsMenu");
+        settingsMenu.GetComponent<Canvas>().enabled = true;
+    }
+    // Starts the game.
+    public void StartButton () {
+        NetworkManager.singleton.StartClient();
+        NetworkManager.singleton.StartHost();
+    }
+}
---
+++ Assets/SuperReaperAssets/MenuPackage/Menu_Scripts/MenuInputHandler.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInputHandler : MonoBehaviour {
+
+    private Canvas pauseMenu;
+    private Canvas settingsMenu;
+
+    // Use this for initialization.
+    void Start () {
+        settingsMenu = GameObject.Find("SettingsMenu").GetComponent<Canvas>();
+        settingsMenu.enabled = false;
+
+        pauseMenu = GameObject.Find("PauseMenu").GetComponent<Canvas>();
+        pauseMenu.enabled = false;
+    }
+
+	// Update is called once per frame.
+	void Update () {
+        if (Input.GetButtonUp("Pause"))
+        {
+            PauseHandler();
+            Debug.Log("Pause Button Pressed");
+        }
+    }
+
+    // Handles changing the currently active pause menus.
+    void PauseHandler () {
+        if (!pauseMenu.enabled && !settingsMenu.enabled && SceneManager.GetActiveScene().name != "MainMenu")
+        {
+            pauseMenu.enabled = true;
+            Debug.Log("PauseMenu enabled");
+        }
+        else if (settingsMenu.enabled)
+        {
+            settingsMenu.enabled = false;
+            Debug.Log("SettingsMenu disabled");
+        }
+        else if (pauseMenu.enabled)
+        {
+            pauseMenu.enabled = false;
+            Debug.Log("PauseMenu disabled");
+        }
+    }
+}
